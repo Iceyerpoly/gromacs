178431e683c750de27fd97bccc75460b33cbba25
zc62 <17048823+zc62@users.noreply.github.com>
2023-06-28 20:08:26 -0500

    Add numpy support for PySCF QMMM interface

--- cmake/gmxManagePySCF.cmake
+++ cmake/gmxManagePySCF.cmake
@@ -34,17 +34,18 @@
 if(GMX_PYSCF)
 
     # CMake flags for PySCF linking
-    find_package(Python REQUIRED Development)
+    find_package(Python COMPONENTS Development NumPy)
 
     # Check if libpython is found
-    if (Python_Development.Embed_FOUND)
+    if (Python_Development.Embed_FOUND AND Python_NumPy_FOUND)
         # libpython found: add libraries and paths to the respecting GROMACS variables
         message(STATUS "Found libpython in ${Python_LIBRARY_DIRS}")
-        include_directories(SYSTEM "${Python_INCLUDE_DIRS}")
+        message(STATUS "Found numpy include dir: ${Python_NumPy_INCLUDE_DIRS}")
+        include_directories(SYSTEM "${Python_INCLUDE_DIRS} ${Python_NumPy_INCLUDE_DIRS}")
         link_directories(${Python_LIBRARY_DIRS})
         list(APPEND GMX_COMMON_LIBRARIES ${Python_LIBRARIES})
     else()
-        message(FATAL_ERROR "To build GROMACS with PySCF Interface, libpython should be installed")
+        message(FATAL_ERROR "To build GROMACS with PySCF Interface, libpython and numpy should be installed")
     endif()
 
 endif()
